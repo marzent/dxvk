8c27a3f53807d38434844d7cc13c936453efcec2
Marc-Aurel Zent <marc_aurel@me.com>
2021-11-22 00:08:05 +0100

    enable async by default, matching proton behaviour

--- src/dxvk/dxvk_options.cpp
+++ src/dxvk/dxvk_options.cpp
@@ -10,7 +10,7 @@ namespace dxvk {
     useRawSsbo            = config.getOption<Tristate>("dxvk.useRawSsbo",             Tristate::Auto);
     halveNvidiaHVVHeap    = config.getOption<Tristate>("dxvk.halveNvidiaHVVHeap",     Tristate::Auto);
     hud                   = config.getOption<std::string>("dxvk.hud", "");
-    enableAsync           = config.getOption<bool>    ("dxvk.enableAsync",            false);
+    enableAsync           = config.getOption<bool>    ("dxvk.enableAsync",            true);
     numAsyncThreads       = config.getOption<int32_t> ("dxvk.numAsyncThreads",        0);
   }
 
